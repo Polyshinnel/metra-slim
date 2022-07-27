3f18c284cda728afc81502addfd6c964078a13e3
Polyshinnel <polyshinnel@gmail.com>
2022-07-27 21:24:26 +0300

    refresh tkp,add excertion in tkpCreator

--- src/Controllers/Tkp/CreateTkpAuto.php
+++ src/Controllers/Tkp/CreateTkpAuto.php
@@ -5,6 +5,8 @@ namespace App\Controllers\Tkp;
 
 
 use App\Models\TkpAuto;
+use PhpOffice\PhpWord\Exception\CopyFileException;
+use PhpOffice\PhpWord\Exception\CreateTemporaryFileException;
 use PhpOffice\PhpWord\TemplateProcessor;
 
 class CreateTkpAuto
@@ -19,21 +21,27 @@ class CreateTkpAuto
         $tkpInfo = TkpAuto::where('id',$tkpId)->first()->toArray();
 
         $template = __DIR__.'../../../../public/assets/tkp'.$tkpInfo['path'];
-        $templateProcessor = new TemplateProcessor($template);
-
-        $templateProcessor->setValue('customerName', $customerName);
-        $templateProcessor->setValue('instalationPlace', $instalationPlace);
-        $templateProcessor->setValue('expiredDate', $expiredDate);
-
-
-
-        $file = 'tkp.docx';
-        $templateProcessor->saveAs($file );
+        try {
+            $templateProcessor = new TemplateProcessor($template);
+            $templateProcessor->setValue('customerName', $customerName);
+            $templateProcessor->setValue('instalationPlace', $instalationPlace);
+            $templateProcessor->setValue('expiredDate', $expiredDate);
 
 
-        header("Content-Disposition: attachment; filename=tkp.docx");
-        readfile($file); // or echo file_get_contents($temp_file);
-        unlink($file); // remove temp file
+            $file = 'tkp.docx';
+            $templateProcessor->saveAs($file);
+
+
+            header("Content-Disposition: attachment; filename=tkp.docx");
+            readfile($file); // or echo file_get_contents($temp_file);
+            unlink($file); // remove temp file
+        } catch (CopyFileException $e) {
+            print_r($e);
+        } catch (CreateTemporaryFileException $e) {
+            print_r($e);
+        }
+
+
 
     }
 }
